86a3da2282b85aacb5bf9502a4a4751e53b2b795
nhonNguyenAbc <nguyenthanhnhonabc@gmail.com>
2024-12-19 20:23:04 +0700

    change api

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Đảm bảo đúng đường dẫn cơ sở
   build: {
     outDir: "dist",
+    chunkSizeWarningLimit: 1000,
   },
 });
